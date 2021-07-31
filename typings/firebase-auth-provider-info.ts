44af5a9af7f1dde7176f8619994461a285aec657
Liau Jian Jie <liaujianjie@gmail.com>
2021-07-31 15:59:12 +0800

    Add type guards for firebase auth types

---
+++ typings/firebase-auth-provider-info.ts
@@ -0,0 +1,36 @@
+// Expand this list as needed.
+const SUPPORTED_AUTH_PROVIDERS = ["google.com", "facebook.com", "twitter.com"] as const;
+const SUPPORTED_AUTH_PROVIDERS_SET = new Set(SUPPORTED_AUTH_PROVIDERS);
+
+type SupportedAuthProviders = typeof SUPPORTED_AUTH_PROVIDERS[number];
+
+type FirebaseAuthProviderInfo = {
+  providerId: SupportedAuthProviders;
+  rawId: string;
+  email?: string;
+  displayName?: string;
+  photoUrl: string;
+};
+
+export function isFirebaseAuthProviderInfo(
+  maybeProviderInfo: any
+): maybeProviderInfo is FirebaseAuthProviderInfo {
+  if (!SUPPORTED_AUTH_PROVIDERS_SET.has(maybeProviderInfo.providerId)) {
+    console.warn(
+      `Unsupported Firebase Auth provider: ${maybeProviderInfo.providerId}. Add to the SUPPORTED_AUTH_PROVIDERS list if needed.`
+    );
+    return false;
+  }
+
+  if (
+    // Check compulsary properties.
+    typeof maybeProviderInfo.rawId !== "string" ||
+    typeof maybeProviderInfo.displayName !== "string" ||
+    typeof maybeProviderInfo.photoUrl !== "string" ||
+    typeof maybeProviderInfo.email !== "string"
+  ) {
+    return false;
+  }
+
+  return true;
+}
